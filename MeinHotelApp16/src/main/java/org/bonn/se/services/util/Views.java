184a1b1e05a4d436ef92e236ebfbfca40a8debc3
Mario Beckel <mario.beckel@smail.inf.h-brs.de>
2016-04-13 09:46:02 +0200

    bis Ende Video 4

---
+++ MeinHotelApp16/src/main/java/org/bonn/se/services/util/Views.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bonn.se.services.util;
+
+/**
+ *
+ * @author Mario<Mario@Mario-PC>
+ */
+public class Views {
+    
+    public static final String MAIN = "main";
+    public static final String LOGIN = "login";
+}
