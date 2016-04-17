bed4b97dbf86a3d6cd278916d77a5be566f707bb
Mario Beckel <mario.beckel@smail.inf.h-brs.de>
2016-04-17 10:36:35 +0200

    bis Ende Video 7 - neu hinzugefuegt

---
+++ MeinHotelApp16/src/main/java/org/bonn/se/services/util/Roles.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bonn.se.services.util;
+
+/**
+ *
+ * @author Mario<Mario@Mario-PC>
+ */
+public class Roles {
+    
+    public static final String CURRENT_USER = "currentUser";
+}
