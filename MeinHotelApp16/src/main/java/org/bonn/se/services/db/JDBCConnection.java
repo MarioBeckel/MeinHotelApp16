37de443902ed61e84215a5430a3f3c5c8991e14c
Mario Beckel <mario.beckel@smail.inf.h-brs.de>
2016-04-24 11:38:12 +0200

    DB-URL geaendert

--- MeinHotelApp16/src/main/java/org/bonn/se/services/db/JDBCConnection.java
+++ MeinHotelApp16/src/main/java/org/bonn/se/services/db/JDBCConnection.java
@@ -28,8 +28,8 @@ public class JDBCConnection {
     
    private static JDBCConnection connection = null;
     
-   private String url = "jdbc:postgresql://dumbo.inf.fh-bonn-rhein-sieg.de/mmoers2s";
-   // private String url = "jdbc:postgresql://localhost//phppgadmin/mbeck12s";
+    private String url = "jdbc:postgresql://dumbo.inf.fh-bonn-rhein-sieg.de/mmoers2s";
+   // private String url = "jdbc:postgresql://localhost//phppgadmin/Mario";
     
     
     private Connection conn;
